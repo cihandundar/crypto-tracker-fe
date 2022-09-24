1e74efdc045f7c3406a20feba6f9a158a93a8491
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-24 19:39:28 +0300

    navbar component added and style edited

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,56 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.webp";
 
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      name: "Cryptocurrencies",
+      path: "/",
+    },
+
+    {
+      id: 2,
+      name: "Exchanges",
+      path: "/",
+    },
+
+    {
+      id: 3,
+      name: "NFT",
+      path: "/",
+    },
+    {
+      id: 4,
+      name: "Learn Crypto",
+      path: "/",
+    },
+    {
+      id: 5,
+      name: "Products",
+      path: "/",
+    },
+  ];
+
   return (
-    <header>
-      <nav></nav>
+    <header className="header">
+      <div className="header__container">
+        <nav className="nav">
+          <div className="nav__logo">
+            <Link to="/">
+              <img src={Logo} alt="" />
+            </Link>
+          </div>
+          <ul className="nav__list">
+            {links.map((link) => (
+              <li key={link.id} className="nav__link">
+                <Link to={link.path}>{link.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
