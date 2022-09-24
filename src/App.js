3c008247b777410314e47a0e7c7b2c3737b05c58
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-24 23:52:18 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Navbar } from "components";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages";
@@ -5,6 +6,7 @@ import { Home } from "./pages";
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
