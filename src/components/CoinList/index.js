787ffb226ec0b6bad99691b0cd2e813137156737
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-24 21:47:41 +0300

    coinlist component header added

--- src/components/CoinList/index.js
+++ src/components/CoinList/index.js
@@ -3,7 +3,17 @@ import React from "react";
 const CoinList = () => {
   return (
     <main className="main">
-      <section className="main__section"></section>
+      <section className="section">
+        <div className="section__container">
+          <header className="section__header">
+            <h2>Cryptocurrency Prices by Market Cap</h2>
+            <p>
+              The global cryptocurrency market cap today is $1.08 Trillion, a
+              5.6% change in the last 24 hours.
+            </p>
+          </header>
+        </div>
+      </section>
     </main>
   );
 };
