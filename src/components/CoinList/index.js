5e6e1a54116931c9ab85424c78919a080b5ffdba
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-25 01:38:55 +0300

    page content and style added

--- src/components/CoinList/index.js
+++ src/components/CoinList/index.js
@@ -4,16 +4,47 @@ import { AiOutlineStar } from "react-icons/ai";
 const CoinList = ({ item }) => {
   return (
     <Link to={item?.id}>
-      <div className="coinlist">
-        <div className="coinlist__left">
+      <div className="section">
+        <div className="section__left">
           <AiOutlineStar />
           <p>{item?.market_cap_rank}</p>
-          <div className="coinlist__left__title">
+          <div className="section__left__title">
             <img src={item?.image} alt="" />
             <h4>{item?.id}</h4>
             <span>{item?.symbol}</span>
           </div>
         </div>
+        <ul className="section__list">
+          <li className="section__list__item">
+            {item.current_price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}
+          </li>
+          <li
+            className={
+              item?.price_change_percentage_24h < 0
+                ? `section__list__item section__list__item--down`
+                : `section__list__item section__list__item--up`
+            }
+          >
+            {item?.price_change_percentage_24h?.toFixed(2)}%
+          </li>
+          <li className="section__list__item">
+            {item.total_volume.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+              maximumFractionDigits: 0,
+            })}
+          </li>
+          <li className="section__list__item">
+            {item.market_cap.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+              maximumFractionDigits: 0,
+            })}
+          </li>
+        </ul>
       </div>
     </Link>
   );
