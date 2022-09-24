3e4d43409cb9e701b29b7812f137480f64d399e9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-24 19:30:12 +0300

    navbar component and fonts added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
 };
 
 export default Home;
