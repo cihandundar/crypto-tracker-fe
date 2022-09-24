e9a39a6482a613154420d1a4ddd28fe78e8177c4
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-24 23:51:43 +0300

    coinlist fix

--- src/components/CoinList/index.js
+++ src/components/CoinList/index.js
@@ -1,7 +1,12 @@
-import { React } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-const CoinList = () => {
-  return <main className="main"></main>;
+const CoinList = ({ item }) => {
+  return (
+    <Link to={item.id}>
+      <h2>{item.id}</h2>
+    </Link>
+  );
 };
 
 export default CoinList;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,65 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import React from "react";
-import { CoinList, Navbar } from "components/";
+import { CoinList } from "components/";
 
 const Home = () => {
+  const titleList = [
+    {
+      id: 7,
+      details: "Price",
+    },
+    {
+      id: 9,
+      details: "24h",
+    },
+    {
+      id: 10,
+      details: "24h Volume",
+    },
+    {
+      id: 11,
+      details: "Mkt Cap",
+    },
+  ];
+
+  const [coin, setCoin] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+      )
+      .then((response) => setCoin(response.data));
+  }, []);
+
   return (
-    <React.Fragment>
-      <Navbar />
-      <CoinList />
-    </React.Fragment>
+    <main className="content">
+      <div className="content__container">
+        <div className="content__header">
+          <h2>Cryptocurrency Prices by Market Cap</h2>
+          <p>
+            The global cryptocurrency market cap today is $1.08 Trillion, a 5.6%
+            change in the last 24 hours.
+          </p>
+        </div>
+        <div className="content__title">
+          <div className="content__title__left">
+            <span>#</span>
+            <p>Coin</p>
+          </div>
+          <div className="content__title__list">
+            {titleList.map((list) => (
+              <li key={list.id} className="content__title__item">
+                {list.details}
+              </li>
+            ))}
+          </div>
+        </div>
+        {coin.map((item) => (
+          <CoinList key={item.id} item={item} />
+        ))}
+      </div>
+    </main>
   );
 };
 
