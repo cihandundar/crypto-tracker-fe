ff94843294a6501d2c5c7befaa3b81ada896542d
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-09-25 01:28:45 +0300

    react icons loaded

--- src/components/CoinList/index.js
+++ src/components/CoinList/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { AiOutlineStar } from "react-icons/ai";
 const CoinList = ({ item }) => {
   return (
     <Link to={item?.id}>
       <div className="coinlist">
         <div className="coinlist__left">
+          <AiOutlineStar />
+          <p>{item?.market_cap_rank}</p>
           <div className="coinlist__left__title">
             <img src={item?.image} alt="" />
             <h4>{item?.id}</h4>
