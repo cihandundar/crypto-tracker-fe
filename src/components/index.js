e490b6cd10760259c77b25d6296a21823af1d12e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-04-10 19:23:02 +0300

    removed

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,5 @@ import Navbar from "./Navbar";
 import CoinList from "./CoinList";
 import HistoryChart from "./HistoryChart";
 import CoinNews from "./CoinNews";
-import Search from "./Search";
-import TrackerList from "./TrackerList";
-export { Navbar, CoinList, HistoryChart, CoinNews, Search, TrackerList };
+import Filter from "./Filter";
+export { Navbar, CoinList, HistoryChart, CoinNews, Filter };
