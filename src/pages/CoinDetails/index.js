038fcdd0ec00af0f16cd13f1780926f0f4ef8c33
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-04-10 19:23:28 +0300

    detail page fix

--- src/pages/CoinDetails/index.js
+++ src/pages/CoinDetails/index.js
@@ -1,33 +1,17 @@
 import React from "react";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { HistoryChart, CoinNews } from "components";
+import { HistoryChart } from "components";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCoinDetails } from "redux/coinSlice";
 
 const CoinDetails = () => {
+  const details = useSelector((state) => state?.coin?.details);
   const { id } = useParams();
-  const [coin, setCoin] = useState({});
-  const [keyword, setKeyword] = useState([]);
-  const [status, setStatus] = useState([]);
-
+  const dispatch = useDispatch();
   useEffect(() => {
-    axios
-      .get(`https://api.coingecko.com/api/v3/coins/${id}`)
-      .then((response) => {
-        setStatus(response);
-        setCoin(response.data);
-      });
-  }, [id]);
-
-  useEffect(() => {
-    if (status.status === 200) {
-      axios
-        .get(
-          `https://cryptonews-api.com/api/v1?tickers=${coin?.symbol}&items=50&page=1&token=5lho08jskwfqyay6sywiorcrigdakrlemtjt64nf`
-        )
-        .then((response) => setKeyword(response.data.data.slice(0, 6)));
-    }
-  }, [coin, status]);
+    dispatch(fetchCoinDetails(id));
+  }, [dispatch, id]);
 
   const detailsList = [
     {
@@ -53,18 +37,17 @@ const CoinDetails = () => {
       <div className="details">
         <div className="details__container">
           <div className="details__rank">
-            <span>Rank #{coin?.market_data?.market_cap_rank}</span>
+            <span>Rank #{details?.market_data?.market_cap_rank}</span>
           </div>
           <div className="details__name">
-            <img src={coin?.image?.small} alt={coin?.name} />
-            <h2>{coin?.name}</h2>
-            <span>{coin?.symbol}</span>
+            <h2>{details?.name}</h2>
+            <span>{details?.symbol}</span>
           </div>
           <ul className="details__list details__list--lower ">
             <li className="details__list--item">
               <h4>Low 24h</h4>
               <p className={"details__list__item details__list__item--down"}>
-                {coin?.market_data?.low_24h.usd?.toLocaleString("en-US", {
+                {details?.market_data?.low_24h.usd?.toLocaleString("en-US", {
                   style: "currency",
                   currency: "USD",
                 })}
@@ -74,7 +57,7 @@ const CoinDetails = () => {
             <li className="details__list--item">
               <h4>High 24h</h4>
               <p className="details__list__item details__list__item--up">
-                {coin?.market_data?.high_24h.usd?.toLocaleString("en-US", {
+                {details?.market_data?.high_24h.usd?.toLocaleString("en-US", {
                   style: "currency",
                   currency: "USD",
                 })}
@@ -92,52 +75,49 @@ const CoinDetails = () => {
             <ul className="details__list details__list--lower ">
               <li
                 className={
-                  coin?.market_data?.price_change_percentage_24h < 0
+                  details?.market_data?.price_change_percentage_24h < 0
                     ? `details__list__item details__list__item--down `
                     : `details__list__item details__list__item--up`
                 }
               >
-                {coin?.market_data?.price_change_percentage_24h?.toFixed(2)}%
+                {details?.market_data?.price_change_percentage_24h?.toFixed(2)}%
               </li>
               <li
                 className={
-                  coin?.market_data?.price_change_percentage_7d < 0
+                  details?.market_data?.price_change_percentage_7d < 0
                     ? `details__list__item details__list__item--down `
                     : `details__list__item details__list__item--up`
                 }
               >
-                {coin?.market_data?.price_change_percentage_7d?.toFixed(2)}%
+                {details?.market_data?.price_change_percentage_7d?.toFixed(2)}%
               </li>
               <li
                 className={
-                  coin?.market_data?.price_change_percentage_30d < 0
+                  details?.market_data?.price_change_percentage_30d < 0
                     ? `details__list__item details__list__item--down`
                     : `details__list__item details__list__item--up`
                 }
               >
-                {coin?.market_data?.price_change_percentage_30d?.toFixed(2)}%
+                {details?.market_data?.price_change_percentage_30d?.toFixed(2)}%
               </li>
               <li
                 className={
-                  coin?.market_data?.price_change_percentage_1y < 0
+                  details?.market_data?.price_change_percentage_1y < 0
                     ? `details__list__item details__list__item--down`
                     : `details__list__item details__list__item--up`
                 }
               >
-                {coin?.market_data?.price_change_percentage_1y?.toFixed(2)}%
+                {details?.market_data?.price_change_percentage_1y?.toFixed(2)}%
               </li>
             </ul>
           </div>
           <article className="details__info">
-            <p dangerouslySetInnerHTML={{ __html: coin?.description?.en }}></p>
+            <p
+              dangerouslySetInnerHTML={{ __html: details?.description?.en }}
+            ></p>
           </article>
           <div className="details__chart">
             <HistoryChart />
-          </div>
-          <div className="details__news">
-            {keyword?.map((news) => (
-              <CoinNews key={news?.id} news={news} />
-            ))}
           </div>
         </div>
       </div>
